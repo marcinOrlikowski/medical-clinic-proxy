c588d7d607580fcd512289dee2b24a87114b99c1
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2026-03-28 11:59:00 +0100

    initial commit

---
+++ src/main/java/com/marcinorlikowski/medicalclinicproxy/MedicalclinicproxyApplication.java
@@ -0,0 +1,13 @@
+package com.marcinorlikowski.medicalclinicproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalclinicproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalclinicproxyApplication.class, args);
+	}
+
+}
